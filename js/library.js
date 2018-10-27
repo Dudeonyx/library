443fab933c53d73d451558743c18796236f4b276
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-10-27 22:02:41 +0100

    making addBookToShelf

--- js/library.js
+++ js/library.js
@@ -1,80 +1,81 @@
+/* eslint-env browser */
 const titleCase = string => string.toLowerCase().replace(/^.| ./g, u => u.toUpperCase()); // eslint-disable-line no-unused-vars
 
 const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
 
-const myObject = (() => {
-  function copy(o, flag) {
-    const output = Array.isArray(o) ? [] : {};
-    Object.keys(o).forEach((key) => {
-      const v = o[key];
-      output[key] = (typeof v === 'object' && flag === true) ? copy(v) : v;
-    });
-    return output;
-  }
-  function merge(objectA = {}, ...objects) {
-    let flag;
-    if (typeof objects[objects.length - 1] === 'boolean') {
-      flag = objects.pop();
+
+const libraryModule = (() => {
+  const myObject = (() => {
+    function copy(o, flag) {
+      const output = Array.isArray(o) ? [] : {};
+      Object.keys(o).forEach((key) => {
+        const v = o[key];
+        output[key] = (typeof v === 'object' && flag === true) ? copy(v) : v;
+      });
+      return output;
     }
-    const newObject = Array.isArray(objectA) ? [] : Object.create(Object.getPrototypeOf(objectA));
-    const propsA = Object.getOwnPropertyNames(objectA);
-    propsA.forEach((prop) => {
-      // if (Array.isArray(objectA) && prop === 'length') return;
-      const desc = Object.getOwnPropertyDescriptor(objectA, prop);
-      if (flag === true && desc.value !== null && typeof desc.value === 'object') {
-        desc.value = merge(desc.value, true);
+    function merge(objectA = {}, ...objects) {
+      let flag;
+      if (typeof objects[objects.length - 1] === 'boolean') {
+        flag = objects.pop();
       }
-      Object.defineProperty(newObject, prop, desc);
-    });
-    objects.forEach((object) => {
-      const props = Object.getOwnPropertyNames(object);
-      props.forEach((prop) => {
-        const desc = Object.getOwnPropertyDescriptor(object, prop);
+      const newObject = Array.isArray(objectA) ? [] : Object.create(Object.getPrototypeOf(objectA));
+      const propsA = Object.getOwnPropertyNames(objectA);
+      propsA.forEach((prop) => {
+        // if (Array.isArray(objectA) && prop === 'length') return;
+        const desc = Object.getOwnPropertyDescriptor(objectA, prop);
         if (flag === true && desc.value !== null && typeof desc.value === 'object') {
           desc.value = merge(desc.value, true);
         }
-        if (prop !== 'constructor' && prop in newObject) {
-          if (!newObject.duplicateProperties) { newObject.duplicateProperties = {}; }
-          Object.defineProperty(newObject.duplicateProperties, prop, desc);
-        } else {
-          Object.defineProperty(newObject, prop, desc);
-        }
+        Object.defineProperty(newObject, prop, desc);
       });
+      objects.forEach((object) => {
+        const props = Object.getOwnPropertyNames(object);
+        props.forEach((prop) => {
+          const desc = Object.getOwnPropertyDescriptor(object, prop);
+          if (flag === true && desc.value !== null && typeof desc.value === 'object') {
+            desc.value = merge(desc.value, true);
+          }
+          if (prop !== 'constructor' && prop in newObject) {
+            if (!newObject.duplicateProperties) { newObject.duplicateProperties = {}; }
+            Object.defineProperty(newObject.duplicateProperties, prop, desc);
+          } else {
+            Object.defineProperty(newObject, prop, desc);
+          }
+        });
+      });
+      return newObject;
+    }
+    function setConstructor(constructor) {
+      return (o, allMethodsPrototypal = false) => {
+        const proto1 = Object.create(Object.getPrototypeOf(o));
+        proto1.constructor = constructor;
+        if (allMethodsPrototypal === true) {
+          return Object.create(merge(proto1, o));
+        }
+        const proto2 = Object.create(proto1);
+        return merge(proto2, o);
+      };
+    }
+    function rClone(o, flag) {
+      const clone = Array.isArray(o) ? [] : {};
+      const props = Object.getOwnPropertyNames(o);
+      props.forEach((prop) => {
+        const desc = Object.getOwnPropertyDescriptor(o, prop);
+        if (flag === true && desc.value !== null && typeof desc.value === 'object') {
+          desc.value = rClone(desc.value, true);
+        }
+        Object.defineProperty(clone, prop, desc);
+      });
+      return clone;
+    }
+    return Object.freeze({
+      merge,
+      setConstructor,
+      rClone,
+      copy,
     });
-    return newObject;
-  }
-  function setConstructor(constructor) {
-    return (o, allMethodsPrototypal = false) => {
-      const proto1 = Object.create(Object.getPrototypeOf(o));
-      proto1.constructor = constructor;
-      if (allMethodsPrototypal === true) {
-        return Object.create(merge(proto1, o));
-      }
-      const proto2 = Object.create(proto1);
-      return merge(proto2, o);
-    };
-  }
-  function rClone(o, flag) {
-    const clone = Array.isArray(o) ? [] : {};
-    const props = Object.getOwnPropertyNames(o);
-    props.forEach((prop) => {
-      const desc = Object.getOwnPropertyDescriptor(o, prop);
-      if (flag === true && desc.value !== null && typeof desc.value === 'object') {
-        desc.value = rClone(desc.value, true);
-      }
-      Object.defineProperty(clone, prop, desc);
-    });
-    return clone;
-  }
-  return Object.freeze({
-    merge,
-    setConstructor,
-    rClone,
-    copy,
-  });
-})();
-
-const Library = (() => {
+  })();
   function LibraryList(libraryList = []) {
     return {
       withAddToLibraryList: o => myObject.merge(o, {
@@ -141,10 +142,10 @@ const Library = (() => {
       }
       function details(detail) {
         const allDetails = {
-          Title: title,
-          Author: author,
-          Pages: pages,
-          Status: status,
+          title,
+          author,
+          pages,
+          status,
           Library: owner,
         };
         return detail ? { [titleCase(detail)]: allDetails[titleCase(detail)] } : allDetails;
@@ -195,5 +196,42 @@ const Library = (() => {
   }
   Library.showLibraryList = showLibraryList;
   // Library.addToLibraryList = addToLibraryList;
-  return Library;
+  return { Library, myObject };
 })();
+
+const { Library } = libraryModule;
+const umeayo = Library('Umeayo');
+const adventure = umeayo.createBook({
+  title: 'Adventures in JS',
+  pages: 256,
+  status: 'Not Read',
+  author: 'Dudeonyx',
+});
+
+function $(element = document) {
+  return selector => element.querySelector(selector);
+}
+function $All(element = document) {
+  return selector => element.querySelectorAll(selector);
+}
+function $Create(element) {
+  return document.createElement(element);
+}
+const shelf = $()('.shelf');
+
+const testBook = $Create('div');
+testBook.classList.add('book');
+shelf.appendChild(testBook);
+const fgh = $(shelf)('.book');
+(function addBookToShelf() {
+  umeayo.displayShelf().forEach((book) => {
+    const { title, author, status, pages } = book.details();
+    const bookElement = $Create('div');
+    bookElement.classList.add('book');
+    const titleField = $Create('p');
+    const authorField = $Create('p');
+    const statusField = $Create('p');
+    const pagesField = $Create('p');
+    
+  });
+}());
